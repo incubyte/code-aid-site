0381985000fa2d601e5d48d442f692eae0d9435f
brijesh-at-incubyte <brijesh@incubyte.co>
2023-08-23 08:54:02 +0530

    update: filter button logic

--- static/security-issues/src/render/filter-buttons.ts
+++ static/security-issues/src/render/filter-buttons.ts
@@ -2,49 +2,63 @@ import { SecurityIssuesHashUrl } from "../security-issue-hash";
 import { GlobalState } from "../state";
 
 export class FilterImpactButtons {
-    constructor(private readonly impactButtonsContainer: HTMLDivElement, allImpacts: string[], private readonly securityIssuesHashUrl: SecurityIssuesHashUrl, private readonly globalState: GlobalState) {
-        allImpacts.forEach((impact) => {
-            const filterButton = document.createElement("button");
-            filterButton.className = "filter-button";
-            filterButton.textContent = `${impact}`;
+  constructor(
+    private readonly impactButtonsContainer: HTMLDivElement,
+    allImpacts: string[],
+    private readonly securityIssuesHashUrl: SecurityIssuesHashUrl,
+    private readonly globalState: GlobalState
+  ) {
+    allImpacts.forEach((impact) => {
+      const filterButton = document.createElement("button");
+      filterButton.className = "filter-button";
+      filterButton.textContent = `${impact}`;
 
-            filterButton.addEventListener("click", function () {
-                filterButton.classList.toggle("selected");
-                if (filterButton.classList.contains("selected")) {
-                    globalState.setImpacts([...globalState.getImpacts(), impact]);
-                }
-                else {
-                    globalState.setImpacts(globalState.getImpacts().filter((imp) => imp !== impact));
-                }
+      filterButton.addEventListener("click", function () {
+        filterButton.classList.toggle("selected");
+        if (filterButton.classList.contains("selected")) {
+          globalState.setImpacts([...globalState.getImpacts(), impact]);
+        } else {
+          globalState.setImpacts(
+            globalState.getImpacts().filter((imp) => imp !== impact)
+          );
+        }
+        globalState.setPageNumber(1);
+        securityIssuesHashUrl.updatePageNumber(globalState.getPageNumber());
+        securityIssuesHashUrl.updateImpacts(globalState.getImpacts());
+      });
 
-                securityIssuesHashUrl.updateImpacts(globalState.getImpacts());
-            });
-
-            impactButtonsContainer.appendChild(filterButton);
-        });
-    }
+      impactButtonsContainer.appendChild(filterButton);
+    });
+  }
 }
 
 export class FilterLanguageButtons {
-    constructor(private readonly languageButtonsContainer: HTMLDivElement, allLanguages: string[], private readonly securityIssuesHashUrl: SecurityIssuesHashUrl, private readonly globalState: GlobalState) {
-        allLanguages.forEach((language) => {
-            const filterButton = document.createElement("button");
-            filterButton.className = "filter-button";
-            filterButton.textContent = `${language}`;
+  constructor(
+    private readonly languageButtonsContainer: HTMLDivElement,
+    allLanguages: string[],
+    private readonly securityIssuesHashUrl: SecurityIssuesHashUrl,
+    private readonly globalState: GlobalState
+  ) {
+    allLanguages.forEach((language) => {
+      const filterButton = document.createElement("button");
+      filterButton.className = "filter-button";
+      filterButton.textContent = `${language}`;
 
-            filterButton.addEventListener("click", function () {
-                filterButton.classList.toggle("selected");
-                if (filterButton.classList.contains("selected")) {
-                    globalState.setLanguages([...globalState.getLanguages(), language]);
-                }
-                else {
-                    globalState.setLanguages(globalState.getLanguages().filter((lang) => lang !== language));
-                }
+      filterButton.addEventListener("click", function () {
+        filterButton.classList.toggle("selected");
+        if (filterButton.classList.contains("selected")) {
+          globalState.setLanguages([...globalState.getLanguages(), language]);
+        } else {
+          globalState.setLanguages(
+            globalState.getLanguages().filter((lang) => lang !== language)
+          );
+        }
+        globalState.setPageNumber(1);
+        securityIssuesHashUrl.updatePageNumber(globalState.getPageNumber());
+        securityIssuesHashUrl.updateLanguages(globalState.getLanguages());
+      });
 
-                securityIssuesHashUrl.updateLanguages(globalState.getLanguages());
-            });
-
-            languageButtonsContainer.appendChild(filterButton);
-        });
-    }
+      languageButtonsContainer.appendChild(filterButton);
+    });
+  }
 }
